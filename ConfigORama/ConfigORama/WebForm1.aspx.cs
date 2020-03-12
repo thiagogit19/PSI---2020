900783d91be6c5423788b97d903de0daac9c264b
†ђเลφ๏ җล√เэя <thiago_xavier.2002@hotmail.com>
2020-03-12 10:23:03 -0300

    Confirmar

---
+++ ConfigORama/ConfigORama/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConfigORama
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                foreach (String strKey in ConfigurationManager.AppSettings.AllKeys)
+                {
+                    this.DropDownList1.Items.Add(strKey);
+                }
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String strSetting;
+            strSetting = ConfigurationManager.AppSettings[
+                this.DropDownList1.SelectedItem.Text];
+            this.Label1.Text = strSetting;
+
+        }
+    }
+}
---
+++ ConfigORama/ConfigORama/WebForm1.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace ConfigORama {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Controle DropDownList1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
+        /// Controle Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Controle Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+    }
+}
